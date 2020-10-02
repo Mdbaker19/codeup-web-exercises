d90e77aef235a4115fc4214f688f75ac18112075
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-02 12:15:08 -0500

    finished do while loops examples

--- JS/loops-lec.js
+++ JS/loops-lec.js
@@ -9,36 +9,69 @@
 
 //TODO TOGETHER: Create a while loop that counts up to 10 (non-inclusive) by 1
 // (0, 1, 2...9)
+
 // let i = 0;
 // while(i < 10){
 //     console.log("count up to '10' -- " + i);
 //     i++;
 // }
 
-
 //TODO TOGETHER: Create a while loop that counts down from 20 to 0 (inclusive) by 1
 // (20, 19, 18...0)
-let i = 20;
-while (i >= 0){
-    console.log("countdown" + i);
-    i--;
-}
 
-
+// let i = 20;
+// while (i >= 0){
+//     console.log("countdown" + i);
+//     i--;
+// }
 
 //TODO TOGETHER: Create a while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
 // (0, 2, 4...20)
 
-
+// let i = 0;
+// while (i <= 20){
+//     console.log("count to '20'--" + i);
+//     i += 2;
+// }
 
 //TODO: Create a while loop that counts to 100 (inclusive) in increments of ten:
 // (10...20...30...)
 
-
+// let i = 10;
+// while(i <= 100) {
+//     console.log("count to '100'--" + i);
+//     i += 10;
+// }
 
 //TODO: Create a while loop that counts down from 50 to 1 (inclusive) in increments of 1:
 // (50, 49, 48, 47, 46 .... 1)
 
+// let i = 50;
+// while(i > 0){
+//     console.log("count to '50'---" + i);
+//     i--;
+// }
+
+//TODO: creat a while loop without numbers:
+    // my attempt below, did not work, if no, can not end it with a yes after
+    // yes initially will end it
+    // need to reassign the confirm
+// let check = confirm("would you like to confirm");
+// while(check === false){
+//     confirm("would you like to confirm");
+//     if(check){
+//         break;
+//     }
+// }
+
+// working with Sophia in class, showing how to do it
+// let count = 0;
+// let addQ = "?";
+// let question = confirm("would you like to exit?");
+// while(!question){
+//     count++
+//     question = confirm("would you like to exit?" + addQ.repeat(count));
+// }
 
 /*********************************************
  *               DO-WHILE LOOPS
@@ -57,20 +90,40 @@ while (i >= 0){
 // 	i++;
 // }
 
-
 // TODO TOGETHER: Why doesnt the example above run?
-
+// i is already not less than 10
 
 // TODO TOGETHER: Refactor the example above using a do-while loop. What behavior do we notice?
-
+// it will log one time with the do
+// let i = 0;
+// do{
+//     console.log("count to '10'--do while loop:" + i);
+//     i++;
+// }while(i < 10);
 
 //TODO TOGETHER: Create a do-while loop that asks the user to confirm: "Would you like to exit?" What difference do you notice?
 
-//TODO TOGETHER: Create a do-while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
-// (0, 2, 4...20)
+// let question = confirm("would you like for me to repeat this?")
+// do{
+//     question = confirm("would you like for me to repeat this?")
+// }while(question);
 
+//TODO TOGETHER: Create a do-while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
+
+// (0, 2, 4...20)
+// let i = 0;
+// do{
+//     console.log("count to '20'-- do while loop" + i);
+//     i += 2;
+// }while(i <=20)
 
 //TODO: Create a do-while loop that prompts the user for a color and continues to prompt until the string passed is "blue"
+
+let correctColor = "blue";
+do{
+    var answer = prompt("What is your favorite color?");
+}while(answer !== correctColor);
+
 
 
 /*********************************************
