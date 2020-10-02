bc985c4926de5dc78facc87b3a6e99ebbc7d0c8e
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-02 15:53:19 -0500

    finished exercises

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -25,3 +25,72 @@ function skipNum(){
 }
 
 //skipNum();
+// var i = 1;
+// while(i < 65535){
+//     i*=2;
+//     console.log(i);
+// }
+
+//const allCones = Math.floor(Math.random() * 50) + 50;
+
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// do{
+//
+//     var conesSold = Math.floor(Math.random() * 5) + 1;
+//     allCones -= conesSold;
+//     console.log(allCones);
+//     if(allCones === 0){
+//         console.log("Yay! I sold them all!")
+//     } else if(allCones >= 0){
+//         console.log("cannot sell you " + conesSold + " I only have " + allCones);
+//     } else {
+//         console.log(allCones + "cones sold...")
+//     }
+// }while(allCones >= 0);
+
+ var allCones = Math.floor(Math.random() * 50) + 50;
+do{
+
+    let conesSold = Math.floor(Math.random() * 5) + 1;
+    allCones -= conesSold;
+    console.log(allCones);
+    if(allCones === 0){
+        console.log("Yay! I sold them all!");
+    } else if(allCones < conesSold){
+        console.log("cannot sell you " + conesSold + " I only have " + allCones);
+    } else {
+        console.log(conesSold + "cones sold...");
+    }
+}while(allCones >= 0);
+
+
+/* assign a max number of cones, decrement it by the random amount sold repeat the selling until you are
+out of cones
+ */
+
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+// function sellingCones(amountOfCones){
+//     var conesSold = Math.floor(Math.random() * 5) + 1;
+//     //let remainingCones = allCones - conesSold;
+//     for(var i = amountOfCones; i > 0; (i-conesSold)){
+//         console.log(i);
+//     }
+// }
+//
+//
+// sellingCones(allCones);
+
+
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+// do{
+//
+//     var conesSold = Math.floor(Math.random() * 5) + 1;
+//     allCones -= conesSold;
+//     console.log(conesSold);
+//
+//
+// }while(allCones >= 0);
+
+
