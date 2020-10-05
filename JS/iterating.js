5b49e46df2f4cf7eaeccaa2367eb93e89a9a1dbd
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-05 15:20:07 -0500

    finished iterating arrays exercise

--- JS/iterating.js
+++ JS/iterating.js
@@ -7,11 +7,15 @@
      * 'names'.
      */
 
+    var names = ["matt", "mandy", "connor", "cindy"];
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+
+    console.log("the names array is : " + names.length + " names long");
 
     /**
      * TODO:
@@ -19,16 +23,27 @@
      * accessing each element's index.
      */
 
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for(let i = 0; i < names.length; i++){
+        console.log(names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
+    names.forEach(name => console.log("done with a forEach loop " + name));
 
     /**
      * TODO:
@@ -43,5 +58,21 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+    function first(array){
+        console.log(array[0]);
+    }
+
+    function second(array){
+        console.log(array[1]);
+    }
+
+    function last(array){
+        console.log(array[array.length - 1]);
+    }
+
+    first([1, 2, 3, 4, 5]) // returns 1
+    second([1, 2, 3, 4, 5]) // returns 2
+    last([1, 2, 3, 4, 5]) // return 5
 
 })();
