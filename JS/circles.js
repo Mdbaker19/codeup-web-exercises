7dec0590ee713043f20e1c04f125ad4cac992ad7
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-09 11:54:01 -0500

    separated out the two different circles to log thm separately

--- JS/circles.js
+++ JS/circles.js
@@ -34,9 +34,33 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
+
+    var circle = {
+        radius: 5,
+
+        getArea: function (x) {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            return Math.PI * Math.pow(x, 2); // TODO: return the proper value
+        },
+
+        otherLogInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+            if(doRounding) {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea(this.radius)));
+            } else {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea(this.radius));
+            }
+        }
+    };
+
     // log info about the circle
     console.log("Raw circle information");
-    circle.logInfo(false);
+    circle.otherLogInfo(false);
     console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+    circle.otherLogInfo(true);
 })();
