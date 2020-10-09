188a210bdd8c845a115bc120badc95deb15e75a0
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-09 14:21:38 -0500

    finsihed math objects exercise

--- JS/circles.js
+++ JS/circles.js
@@ -33,34 +33,11 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
-
-    var circle = {
-        radius: 5,
-
-        getArea: function (x) {
-            // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            return Math.PI * Math.pow(x, 2); // TODO: return the proper value
-        },
-
-        otherLogInfo: function (doRounding) {
-            // TODO: complete this method.
-
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-            if(doRounding) {
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea(this.radius)));
-            } else {
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea(this.radius));
-            }
-        }
-    };
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
-    circle.otherLogInfo(false);
+    circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
-    circle.otherLogInfo(true);
+    circle.logInfo(true);
 })();
