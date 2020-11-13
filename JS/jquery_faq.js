df0fbe4ebcbf17c03bbd977c9f0e0da46deb5f9d
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-11-13 10:59:55 -0600

    added a highlighted class when a dt tag is clicked

--- JS/jquery_faq.js
+++ JS/jquery_faq.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $("#toggleDD").on("click", function (){
         $("dd").toggleClass("invisible");
     });
+    $("dt").on("click", function (){
+        if($(this).hasClass("highlighted")){
+            $(this).removeClass("highlighted");
+        } else $(this).addClass("highlighted");
+    });
 });
